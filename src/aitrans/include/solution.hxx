ada343c375666cc557dc9f53464459daecd6efa0
jie <1584648244@qq.com>
2020-07-25 23:54:56 +0800

    fix spell mistake(block_size=>blocks_size); normalization： files to be given to players: solution.cxx, solution.hxx

--- src/aitrans/include/solution.hxx
+++ src/aitrans/include/solution.hxx
@@ -8,19 +8,14 @@
 using namespace std;
 static map<string, uint64_t> your_parameter;
 extern "C" {
-    struct AckInfo {
-        char *event_type;
-    };
     struct Blocks {
         uint64_t *blocks_id, *blocks_deadline, *blocks_priority,
-            *blocks_create_time;
+            *blocks_create_time, *blocks_size;
         uint64_t block_num;
     };
+    void SolutionInit();
     uint64_t SolutionSelectPacket(struct Blocks blocks, uint64_t current_time);
-    uint64_t CSelectBlock(char *blocks_string, uint64_t block_num,
-                        uint64_t current_time);
-    uint64_t SolutionCcTrigger(AckInfo* ack_infos, uint64_t ack_num, uint64_t cwnd);
-    uint64_t Ccc_trigger(AckInfo* ack_infos, uint64_t ack_num, uint64_t cwnd);
+    uint64_t SolutionCcTrigger(char *event_type, uint64_t rtt, uint64_t bytes_in_flight, uint64_t cwnd);
 }
 
 #endif /* AITRANS_H_ */
--- src/aitrans/solution.cxx
+++ src/aitrans/solution.cxx
@@ -1,50 +1,40 @@
-#include "aitrans.hxx"
+#include "solution.hxx"
 
-// struct Blocks {
-//   uint64_t *blocks_id, *blocks_deadline, *blocks_priority,
-//   *blocks_create_time; uint64_t block_num;
-// };
-// struct AckInfo {
-//     char *event_type;
-// };
+void SolutionInit(){
+    your_parameter["max_packet_size"] = 1350;
+    your_parameter["init_ssthresh"] = 2 * your_parameter["max_packet_size"];
+    your_parameter["last_time"]=0;
+    your_parameter["ssthresh"] = your_parameter["init_ssthresh"];
+}
 
 uint64_t SolutionSelectPacket(struct Blocks blocks, uint64_t current_time) {
     /************** START CODE HERE ***************/
     // return the id of block you want to send, for example:
-    if(your_parameter.count("last_time") > 0){
-        uint64_t last_time = your_parameter["last_time"];
-        fprintf(stderr,"last_time = %lu, current_time = %lu\n", last_time, current_time);
-    }
+    uint64_t last_time = your_parameter["last_time"];
+    fprintf(stderr,"last_time = %lu, current_time = %lu\n", last_time, current_time);
     
     your_parameter["last_time"] = current_time;
     return blocks.blocks_id[0];
     /************** END CODE HERE ***************/
 }
 
-uint64_t SolutionCcTrigger(AckInfo *ack_infos, uint64_t ack_num, uint64_t cwnd) {
+uint64_t SolutionCcTrigger(char *event_type, uint64_t rtt, uint64_t bytes_in_flight, uint64_t cwnd) {
     /************** START CODE HERE ***************/
-    for(uint64_t i=0;i<ack_num;i++){
-        char* event_type = ack_infos[i].event_type;
-        fprintf(stderr, "event_type=%s\n", event_type);
-        const uint64_t max_packet_size = 1350;
-        const uint64_t init_ssthresh = 2 * max_packet_size;
-        if (your_parameter.count("ssthresh") <= 0)
-            your_parameter["ssthresh"] = init_ssthresh;
-        // return new cwnd, for example:
-        uint64_t ssthresh = your_parameter["ssthresh"];
-        if (event_type[11] == 'F') {  // EVENT_TYPE_FINISHED
-            if (cwnd > ssthresh)
-                cwnd += max_packet_size / 2;
-            else {
-                cwnd += max_packet_size;
-            }
+    fprintf(stderr, "rtt: %lu, bytes_in_flight: %lu\n", rtt, bytes_in_flight);
+    // return new cwnd, for example:
+    uint64_t ssthresh = your_parameter["ssthresh"];
+    if (event_type[11] == 'F') {  // EVENT_TYPE_FINISHED
+        if (cwnd > ssthresh)
+            cwnd += your_parameter["max_packet_size"] / 2;
+        else {
+            cwnd += your_parameter["max_packet_size"];
         }
-        if (event_type[11] == 'D') {
-            cwnd = cwnd / 2;  // EVENT_TYPE_DROP
-            your_parameter["ssthresh"] = cwnd;
-        }
-        fprintf(stderr,"new cwnd: %lu, ssthresh = %lu\n", cwnd, your_parameter["ssthresh"]);
     }
+    if (event_type[11] == 'D') {
+        cwnd = cwnd / 2;  // EVENT_TYPE_DROP
+        your_parameter["ssthresh"] = cwnd;
+    }
+    fprintf(stderr,"new cwnd: %lu, ssthresh = %lu\n", cwnd, your_parameter["ssthresh"]);
     return cwnd;
     /************** END CODE HERE ***************/
 }
