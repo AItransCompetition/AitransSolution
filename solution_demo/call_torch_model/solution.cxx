75d8d63501aefa11334861fec1ca2db7841ed18a
Azson <240326315@qq.com>
2020-09-04 15:15:06 +0800

    init demo for calling torch model

---
+++ solution_demo/call_torch_model/solution.cxx
@@ -0,0 +1,84 @@
+#include <torch/script.h> // One-stop header.
+
+#include <iostream>
+#include <memory>
+
+#include "solution.hxx"
+
+void SolutionInit()
+{
+    your_parameter["max_packet_size"] = 1350;
+    your_parameter["init_ssthresh"] = 2 * your_parameter["max_packet_size"];
+    your_parameter["last_time"] = 0;
+    your_parameter["ssthresh"] = your_parameter["init_ssthresh"];
+
+    torch::jit::script::Module module;
+    try
+    {
+        // Deserialize the ScriptModule from a file using torch::jit::load().
+        module = torch::jit::load("my_module_model.pt");
+
+        // Create a vector of inputs.
+        std::vector<torch::jit::IValue> inputs;
+        inputs.push_back(torch::ones({5, 10}));
+
+        // Execute the model and turn its output into a tensor.
+        at::Tensor output = module.forward(inputs).toTensor();
+        std::cout << output << '\n';
+    }
+    catch (const c10::Error &e)
+    {
+        std::cerr << "error loading the model\n";
+        return -1;
+    }
+
+    std::cout << "ok\n";
+}
+
+uint64_t SolutionSelectBlock(Block *blocks, uint64_t block_num, uint64_t current_time)
+{
+    /************** START CODE HERE ***************/
+    // return the id of block you want to send, for example:
+    uint64_t last_time = your_parameter["last_time"];
+    // fprintf(stderr,"last_time = %lu, current_time = %lu\n", last_time, current_time);
+
+    your_parameter["last_time"] = current_time;
+    return blocks[0].block_id;
+    /************** END CODE HERE ***************/
+}
+
+void SolutionCcTrigger(CcInfo *cc_infos, uint64_t cc_num, uint64_t *congestion_window, uint64_t *pacing_rate)
+{
+    /************** START CODE HERE ***************/
+    uint64_t cwnd = *congestion_window;
+    for (uint64_t i = 0; i < cc_num; i++)
+    {
+        char event_type = cc_infos[i].event_type;
+        // fprintf(stderr, "event_type=%c\n", event_type);
+        const uint64_t max_packet_size = 1350;
+        const uint64_t init_ssthresh = 2 * max_packet_size;
+        if (your_parameter.count("ssthresh") <= 0)
+            your_parameter["ssthresh"] = init_ssthresh;
+        // return new cwnd, for example:
+        uint64_t ssthresh = your_parameter["ssthresh"];
+        if (event_type == 'F')
+        { // EVENT_TYPE_FINISHED
+            if (cwnd > ssthresh)
+                cwnd += max_packet_size / 2;
+            else
+            {
+                cwnd += max_packet_size;
+            }
+        }
+        if (event_type == 'D')
+        {
+            cwnd = cwnd / 2; // EVENT_TYPE_DROP
+            your_parameter["ssthresh"] = cwnd;
+        }
+        // fprintf(stderr,"new cwnd: %lu, ssthresh = %lu\n", cwnd, your_parameter["ssthresh"]);
+    }
+    // fprintf(stderr,"new cwnd: %lu, ssthresh = %lu\n", cwnd, your_parameter["ssthresh"]);
+    *pacing_rate = 123456789;
+    *congestion_window = cwnd;
+    /************** END CODE HERE ***************/
+}
